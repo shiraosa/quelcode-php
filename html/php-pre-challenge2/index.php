0e7e1473478213f107afc1df50dd7e01efdf39f7
shiraosa <tezukatrigger@gmail.com>
2020-05-22 12:18:53 +0900

    二回目のfor文のインデックスを$jに変更しました。

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -1,15 +1,15 @@
 <?php
-$array = explode(',', $_GET['array']);
+$array=explode(',', $_GET['array']);
 
 // 修正はここから
 $count=count($array);
-for ($i=0; $i<$count; $i++) {
+for ($i=0;$i<$count;$i++) {
     if(count(array_keys($array,min($array)))>1){//最小値が二つ以上ある場合
-        $for_count=$i+count(array_keys($array,min($array)));//カウント上限の変数
-        for ($i;$i<$for_count;$i++){
+        $for_count=count(array_keys($array,min($array)));//最小値の個数を変数に代入
+        for ($j=0;$j<$for_count;$j++){
             $min_sort[]=min($array);//最小値を代入
         }
-        $i--;//$iのカウント重複を防ぐ
+        $i+=$j-1;//$iに$jカウントの反映とカウントの重複を-1する
     }else{
         $min_sort[]=min($array);//最小値を代入
     }
