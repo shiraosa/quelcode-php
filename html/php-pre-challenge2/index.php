fea06aa1cc607dffa3b436a1ec2943d647e59d9e
shiraosa <tezukatrigger@gmail.com>
2020-05-22 10:22:29 +0900

    最小値を代入して配列から最小値を削除を繰り返す。

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,9 +2,20 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-
+$count =count($array);
+for ($i = 0; $i < $count; $i++) {
+    if(count(array_keys($array,min($array)))>1){//最小値が二つ以上ある場合
+        $arraycount =$i+count(array_keys($array,min($array)));//カウント上限の変数
+        for ($i;$i<$arraycount;$i++){
+            $minsort[]=min($array);//最小値を代入
+        }
+        $i--;//$iのカウント重複を防ぐ
+    }else{
+    $minsort[]=min($array);//最小値を代入
+    }
+    $array =array_diff($array,array(min($array)));//配列から最小値を削除
 }
+$array=$minsort;
 // 修正はここまで
 
 echo "<pre>";
