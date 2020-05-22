44e9c90ed484a45771df8e02fcdef0f7c751d092
shiraosa <tezukatrigger@gmail.com>
2020-05-22 10:32:17 +0900

    変数名を変更しました

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -5,17 +5,17 @@ $array = explode(',', $_GET['array']);
 $count =count($array);
 for ($i = 0; $i < $count; $i++) {
     if(count(array_keys($array,min($array)))>1){//最小値が二つ以上ある場合
-        $arraycount =$i+count(array_keys($array,min($array)));//カウント上限の変数
-        for ($i;$i<$arraycount;$i++){
-            $minsort[]=min($array);//最小値を代入
+        $for_count =$i+count(array_keys($array,min($array)));//カウント上限の変数
+        for ($i;$i<$for_count;$i++){
+            $min_sort[]=min($array);//最小値を代入
         }
         $i--;//$iのカウント重複を防ぐ
     }else{
-    $minsort[]=min($array);//最小値を代入
+    $min_sort[]=min($array);//最小値を代入
     }
     $array =array_diff($array,array(min($array)));//配列から最小値を削除
 }
-$array=$minsort;
+$array=$min_sort;
 // 修正はここまで
 
 echo "<pre>";
