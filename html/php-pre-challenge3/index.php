ec129b70e91fca1544e629eb1508dfac34420ac8
shiraosa <tezukatrigger@gmail.com>
2020-06-01 15:09:40 +0900

    １や組み合わせのない大きな数字で動作確認

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -2,7 +2,6 @@
 $limit = $_GET['target'];
 settype($limit,"int");
 
-
 $dsn = 'mysql:dbname=test;host=mysql';
 $dbuser = 'test';
 $dbpassword = 'test';
@@ -14,76 +13,57 @@ try{
 $dbdata = $db->query("SELECT * FROM prechallenge3");
 $prechallenge3 = $dbdata -> fetchAll();
 $value = array_column($prechallenge3,"value");
-
 rsort($value);
 $count=count($value);
 for($i=0; $i < $count;$i++){
     settype($value[$i],"int");
 }
 
-function array_json($value,$limit,$diff,$i,$j,$k,$check,$json,$answer){
+function array_json($value,$limit,$diff,$i,$j,$k,$json,$answer){
     global $json;
     $range = array_slice($value,$i+$j);
     $max = $range[$k];
-    if($j==1){
+    if($limit=== $value[$i]){
+        $json[] = [$value[$i]];
+        unset($answer);
+        $i++;
+        array_json($value,$limit,$diff,$i,$j,$k,$json,$answer);
+    }
+    if($j===1){
         $answer=[$value[$i]];
         $diff = $limit - $value[$i];
     }
-    if($i < count($value)){
-        if($diff>array_sum($range)&&$k<count($value)){
+    if($i < count($value)){//関数を終わらせるかどうか
+        if($diff>array_sum($range) && $k < count($value)){
             unset($answer);
+            $j=1;
             $k++;
-            $j=1;
-            //スタートへ
-        }elseif($diff>array_sum($range)){
+        }elseif($diff>array_sum($range) or $diff < 0){
             unset($answer);
-            $k = 0;
             $i++;
-            $j=1;
-            //スタートへ
+            $j = 1;
+            $k = 0;
         }elseif($diff<$max){
             $j++;
-            //スタートへ
-        }elseif($diff>$max && in_array($max,array($check))){
-            $check = array_diff($check,$max);
-            $range = array_slice($value,$j++);
-            $max = $range[$k];
-            if($diff>array_sum($range)){
-                unset($answer);
-                $j = 1;
-                //スタートへ
-            }elseif($diff<$max){
-                $j++;
-                //スタートへ
-            }elseif($diff>$max){
-                $diff = $diff - $max;
-                $j++;
-                $answer[] = $max;
-                //スタートへ
-            }
         }elseif($diff>$max){
             $diff = $diff - $max;
             $j++;
             $answer[] = $max;
-            //スタートへ
         }elseif($diff - $max===0 ){
             $answer[] = $max;
             if(isset($json)&&in_array($answer,$json) ){
                 $k++;
-                $j=1;
-                unset($answer);
-                //スタートへ
+                array_pop($answer);
             }else{
                 $json[] = $answer;
-                $check = $answer;
                 unset($answer);
                 $j = 1;
-                //スタートへ
             }
         }
-        array_json($value,$limit,$diff,$i,$j,$k,$check,$json,$answer);
+        array_json($value,$limit,$diff,$i,$j,$k,$json,$answer);
     }
 }
 
-array_json($value,$limit,$diff,0,1,0,$check,$json,$answer);
+array_json($value,$limit,$diff,0,1,0,$json,$answer);
+
 echo(json_encode($json));
