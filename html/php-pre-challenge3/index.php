6fce5154b26a09098b829b7554342546897be91d
shiraosa <tezukatrigger@gmail.com>
2020-06-04 10:34:55 +0900

    スペースの統一

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -1,7 +1,7 @@
 <?php
 //targetが正の整数か確認
 $limit = $_GET['target'];
-if(! ctype_digit($limit) || substr("$limit",0,1)==0){
+if(! ctype_digit($limit) || substr("$limit",0,1) === "0"){
     http_response_code(400);
     exit();
 }
@@ -19,40 +19,40 @@ try{
 }
 
 //DBの配列をソートと型変換
-$dbdata = $db->query("SELECT * FROM prechallenge3 ORDER BY value DESC");
+$dbdata = $db -> query("SELECT * FROM prechallenge3 ORDER BY value DESC");
 $prechallenge3 = $dbdata -> fetchAll();
-$value = array_column($prechallenge3, "value");
-$value_count = count($value);
-for($i = 0; $i < $value_count; $i++){
-    settype($value[$i],"int");
+$values = array_column($prechallenge3, "value");
+$values_count = count($values);
+for($i = 0; $i < $values_count; $i++){
+    settype($values[$i], "int");
 }
 
 //合計値が$limitになる組み合わせを$jsonに保存する関数
-function array_json($value, $limit, $i=0, $j=1, $k=0,$json=null,$diff=null,$answer=null,$check=null){
+function array_json($values, $limit, $i = 0, $j = 1, $k = 0, $json = array(), $diff = null, $answer = null, $check = null){
     //$json;合計値が$limitになる組み合わせを保存する変数
     //$diff;差を保存する変数
     //$answer;組み合わせを一時的に保存する変数
     //$check;取りこぼしがないか確認する変数
-    if($limit === $value[$i]){//組み合わせなしで$limitになる場合
-        $json[] = [$value[$i]];
+    if($limit === $values[$i]){//組み合わせなしで$limitになる場合
+        $json[] = [$values[$i]];
         unset($answer);
         $i++;
-        return array_json($value, $limit, $i, $j, $k, $json, $diff, $answer, $check);
+        return array_json($values, $limit, $i, $j, $k, $json, $diff, $answer, $check);
     }
     if($j === 1){
-        $answer = [$value[$i]];
-        $diff = $limit - $value[$i];
+        $answer = [$values[$i]];
+        $diff = $limit - $values[$i];
     }
-    $range = array_slice($value, $i+$j);//配列の範囲を絞る
+    $range = array_slice($values, $i+$j);//配列の範囲を絞る
     $max = $range[$k];//絞られた配列の中で最大の数値
-    if($i < count($value)){//関数を終わらせるかどうか
+    if($i < count($values)){//関数を終わらせるかどうか
         if($diff < 0){
             unset($answer);
             $i++;
             $j = 1;
             $k = 0;
         }elseif($diff > array_sum($range)){
-            if($k < count($value)){
+            if($k < count($values)){
                 unset($answer);
                 $j = 1;
                 $k++;
@@ -77,7 +77,7 @@ function array_json($value, $limit, $i=0, $j=1, $k=0,$json=null,$diff=null,$answ
             $answer[] = $max;
         }elseif($diff === $max){//合計値がlimitになった場合
             $answer[] = $max;
-            if(isset($json)&&in_array($answer,$json)){//target12で[7,5]が得られた時に[7,4,1]を取りこぼさない措置
+            if(isset($json) && in_array($answer,$json)){//target12で[7,5]が得られた時に[7,4,1]を取りこぼさない措置
                 $j++;
                 array_pop($answer);
             }else{
@@ -87,11 +87,11 @@ function array_json($value, $limit, $i=0, $j=1, $k=0,$json=null,$diff=null,$answ
                 $j = 1;
             }
         }
-        return array_json($value, $limit, $i, $j, $k, $json, $diff, $answer, $check);
+        return array_json($values, $limit, $i, $j, $k, $json, $diff, $answer, $check);
     }
     return $json;
 }
 
-$out_json = array_json($value, $limit);
+$out_json = array_json($values, $limit);
 //$out_jsonをjson形式で出力
 echo(json_encode($out_json));
