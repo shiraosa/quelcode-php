248518202d724a8e138f83945d5bcf5e6c736d71
shiraosa <tezukatrigger@gmail.com>
2020-06-08 12:15:30 +0900

    全ての組み合わせを取得後に$limitと照合する方式に変更しました。

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -1,7 +1,7 @@
 <?php
 //targetが正の整数か確認
 $limit = $_GET['target'];
-if(! ctype_digit($limit) || substr("$limit",0,1) === "0"){
+if(! ctype_digit($limit) || substr("$limit", 0, 1) === "0"){
     http_response_code(400);
     exit();
 }
@@ -27,73 +27,43 @@ for($i = 0; $i < $values_count; $i++){
     settype($values[$i], "int");
 }
 
-//合計値が$limitになる組み合わせを$jsonに保存する関数
-function array_json($values, $limit, $i = 0, $j = 1, $k = 0, $json = array(), $diff = null, $answer = null, $check = null){
-    //$json;合計値が$limitになる組み合わせを保存する変数
-    //$diff;差を保存する変数
-    //$answer;組み合わせを一時的に保存する変数
-    //$check;取りこぼしがないか確認する変数
-    if($limit === $values[$i]){//組み合わせなしで$limitになる場合
-        $json[] = [$values[$i]];
-        unset($answer);
-        $i++;
-        return array_json($values, $limit, $i, $j, $k, $json, $diff, $answer, $check);
-    }
-    if($j === 1){
-        $answer = [$values[$i]];
-        $diff = $limit - $values[$i];
-    }
-    $range = array_slice($values, $i+$j);//配列の範囲を絞る
-    $max = $range[$k];//絞られた配列の中で最大の数値
-    if($i < count($values)){//関数を終わらせるかどうか
-        if($diff < 0){
-            unset($answer);
-            unset($check);
-            $i++;
-            $j = 1;
-            $k = 0;
-        }elseif($diff > array_sum($range)){
-            if($k < count($values)){
-                unset($answer);
-                $j = 1;
-                $k++;
-            }else{
-                unset($answer);
-                unset($check);
-                $i++;
-                $j = 1;
-                $k = 0;
-            }
-        }elseif($diff < $max){
-            if(isset($check) && in_array(min($answer),$check)){//target13で[7,5.1]が得られた時に[7,2,3,1]を取りこぼさない措置
-                $del_key = array_search(min($answer),$check);
-                unset($check[$del_key]);
-                $diff = $diff + min($answer);
-                array_pop($answer);
-            }else{
+//https://stabucky.com/wp/archives/2188 を利用しました。変数を理解しやすいように変えてあります。組み合わせをツリーにした際に階層が進むにつれて枝が少なくなるのをarray_sliceで再現したものだと理解してます。
+function array_combi($values, $pick){
+    $count_values = count($values);
+    if($count_values < $pick ){
+        return;
+    }elseif($pick === 1){
+        for($i =0; $i < $count_values; $i++){
+            $array_combi[$i] = array($values[$i]);
+        }
+    }elseif($pick > 1){
+        $j = 0;
+        for($i = 0; $i < $count_values - $pick + 1; $i++){
+            $read_array_combi = array_combi(array_slice($values, $i+1), $pick-1);
+            foreach($read_array_combi as $combi){
+                array_unshift($combi, $values[$i]);
+                $array_combi[$j] = $combi;
                 $j++;
-            }
-        }elseif($diff > $max){
-            $diff = $diff - $max;
-            $j++;
-            $answer[] = $max;
-        }elseif($diff === $max){//合計値がlimitになった場合
-            $answer[] = $max;
-            if(isset($json) && in_array($answer,$json)){//target12で[7,5]が得られた時に[7,4,1]を取りこぼさない措置
-                $j++;
-                array_pop($answer);
-            }else{
-                $json[] = $answer;
-                $check = $answer;
-                unset($answer);
-                $j = 1;
             }
         }
-        return array_json($values, $limit, $i, $j, $k, $json, $diff, $answer, $check);
     }
-    return $json;
+    return $array_combi;
 }
 
-$out_json = array_json($values, $limit);
-//$out_jsonをjson形式で出力
-echo(json_encode($out_json));
+//全組み合わせの取得
+$all_array_combi = array();
+for($i = 1; $i <= $values_count; $i++){
+    $array_combi = array_combi($values, $i);
+    $all_array_combi = array_merge($all_array_combi, $array_combi);
+}
+
+//$limitと$checkを照合
+$answer =array();
+foreach($all_array_combi as $check){
+    if(array_sum($check) === $limit){
+        $answer[] = $check; 
+    }
+}
+
+//json形式で出力
+echo(json_encode($answer));
